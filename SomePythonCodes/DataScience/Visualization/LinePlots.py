95f720ca943e205475f72f99a6fbba048bb86f27
Moustafa <farhat.forall@gmail.com>
2019-05-20 23:05:09 +0200

    Add project files.

---
+++ SomePythonCodes/DataScience/Visualization/LinePlots.py
@@ -0,0 +1,4 @@
+# Some Python Codes
+# the GNU General Public License along with this program. If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+
---
+++ SomePythonCodes/SomePythonCodes.py
@@ -0,0 +1,9 @@
+# Some Python Codes
+# تعلم البايثون من خلال الأمثلة
+# 19/05/2019
+# Author : Moustafa Farhat
+
+print("Some Python Codes")
+print("تعلم البايثون من خلال الأمثلة")
+print("Enjoy Learning")
+
